af0c8ccd171da56357e1c8bfa279177c21cd58e1
Vladimir <v0v.voron2005@yandex.ru>
2023-05-29 19:16:48 +0300

    Add product migration and model

--- database/migrations/2023_05_29_154249_create_products_table.php
+++ database/migrations/2023_05_29_154249_create_products_table.php
@@ -12,6 +12,10 @@ return new class () extends Migration {
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->unsignedBigInteger('price');
+            $table->unsignedBigInteger('count');
             $table->timestamps();
         });
     }
