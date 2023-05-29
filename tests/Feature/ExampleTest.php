974bf6552d4d95bc5a0ab8558a3e900287bab575
Vladimir <v0v.voron2005@yandex.ru>
2023-05-29 20:13:06 +0300

    Update test file

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -10,4 +10,8 @@ class ExampleTest extends TestCase
     /**
      * A basic test example.
      */
+    public function test_the_application_returns_a_successful_response(): void
+    {
+        $this->assertTrue(true);
+    }
 }
