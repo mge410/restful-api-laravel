15fb998c0522f3815898588ffdf10005820bf8e4
Vladimir <v0v.voron2005@yandex.ru>
2023-05-29 19:35:01 +0300

    Add product controllers

---
+++ app/Http/Controllers/Product/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Product;
+
+class IndexController extends BaseController
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function __invoke()
+    {
+        return 111;
+    }
+}
